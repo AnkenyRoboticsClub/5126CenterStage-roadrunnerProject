b0ffe52903244666c37ba3caad24ba333270cc0a
SamaraF24 <feldhacker.samara@gmail.com>
2023-11-29 15:58:37 -0600

    Added in MeepMeep and started a program to do RR stuff

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,5 +1,26 @@
 package com.example.meepmeeptesting;
 
-public class MeepMeepTesting {
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
+public class MeepMeepTesting {
+    public static void main(String args[]){
+        MeepMeep mm = new MeepMeep(800);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(mm)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 18)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(13, 61, Math.toRadians(-90)))
+                                .forward(5)
+                                .build()
+                );
+        mm.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
 }
